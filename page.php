d57942a5603def5f439018663d5cc74dc58a1457
sinanisler <sinan@sinanisler.com>
2022-05-10 01:48:10 +0300

    buddypress css

--- page.php
+++ page.php
@@ -2,15 +2,23 @@
 
 
 
+
+<div class="category-header" >
+<div class="container" style="font-size:30px; margin-top:20px">
+    <h1 class="category-header-title"><?php the_title(); ?></h1>
+</div>
+</div>
+
+
+
+
 <div class="container">
     <div class="row">
-        <section class="col-md-10 animate__animated animate__fadeIn">
+        <section class="col-md-12 animate__animated animate__fadeIn">
                 
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article>
-                    <div class="forum-post ">
-                        <h1><?php the_title(); ?></h1>
-                    </div>
+                    
                     <div class="forum-post-content">
                         <?php the_content(); ?>
 
