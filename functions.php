e9418ae87898599388057ac1531a31095967d7ee
Sinan <sinan@sinanisler.com>
2022-06-16 17:57:39 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,8 +4,6 @@
 
 
 
-
-
 // Add Menu
 add_theme_support( 'menus' );
 register_nav_menus(
@@ -15,10 +13,11 @@ register_nav_menus(
 );
 
 
-// Add Widgets
+// Add Widgets 
+/*
 register_sidebars( 1, array( 'name' => 'Sidebar_Single' ) );
 register_sidebars( 1, array( 'name' => 'Sidebar_Index' ) );
-
+*/
 
 
 
@@ -63,7 +62,7 @@ add_filter( 'login_headerurl', 'custom_loginlogo_url' );
 
 
 
-// Allow most of the html tags like the_content output
+// Allowing html tags like the_content output
 function filter_html_usage() {
 	global $allowedtags;
 	$allowedtags['code']       = array( 'class' => array() );
@@ -89,7 +88,8 @@ add_action( 'init', 'filter_html_usage', 11 );
 
 
 
-// Settings
+// Theme Settings 
+/*
 function siforum_customize_register( $wp_customize ) {
 
 	$wp_customize->add_section(
@@ -330,16 +330,15 @@ function siforum_customize_register( $wp_customize ) {
 
 add_action( 'customize_register', 'siforum_customize_register' );
 
+*/
 
 
 
 
-
-// CATEGORY ICONS
+// CATEGORY ICONS and COLORS
 
 function wcr_category_fields( $term ) {
-	// we check the name of the action because we need to have different output
-	// if you have other taxonomy name, replace category with the name of your taxonomy. ex: book_add_form_fields, book_edit_form_fields
+
 	if ( current_filter() == 'category_edit_form_fields' ) {
 		$icon_slug  = get_term_meta( $term->term_id, 'icon_slug', true );
 		$color_code = get_term_meta( $term->term_id, 'color_code', true );
@@ -373,10 +372,9 @@ function wcr_category_fields( $term ) {
 		</div>
 		<?php
 	}
+
 }
 
-// Add the fields, using our callback function
-// if you have other taxonomy name, replace category with the name of your taxonomy. ex: book_add_form_fields, book_edit_form_fields
 add_action( 'category_add_form_fields', 'wcr_category_fields', 10, 2 );
 add_action( 'category_edit_form_fields', 'wcr_category_fields', 10, 2 );
 
@@ -391,8 +389,6 @@ function wcr_save_category_fields( $term_id ) {
 	}
 }
 
-// Save the fields values, using our callback function
-// if you have other taxonomy name, replace category with the name of your taxonomy. ex: edited_book, create_book
 add_action( 'edited_category', 'wcr_save_category_fields', 10, 2 );
 add_action( 'create_category', 'wcr_save_category_fields', 10, 2 );
 
@@ -442,7 +438,7 @@ function atarikafa_comments( $comment, $args, $depth ) {
 					$comment->comment_ID
 				);
 				?>
-												</span>
+				</span>
 			</div>
 		</div>
 	<?php
@@ -452,7 +448,7 @@ function atarikafa_comments( $comment, $args, $depth ) {
 
 
 
-
+// Security - Removing some Actions for normal users
 add_action( 'bulk_actions-edit-comments', 'author_remove_comments_actions' );
 add_action( 'comment_row_actions', 'author_remove_comments_actions' );
 
@@ -486,7 +482,7 @@ function author_remove_comments_actions( $actions ) {
 }
 
 
-
+// Security - Hiding comment IPs for normal users
 function lp_remove_IP_for_user( $comment_author_IP, $comment_ID, $comment ) {
 
 	if ( ! is_admin() ) {
@@ -512,7 +508,7 @@ add_filter( 'get_comment_author_IP', 'lp_remove_IP_for_user', 10, 3 );
 
 
 
-
+// Security - Hiding others posts for user to see
 function only_show_own_author_posts( $wp_query ) {
 	if ( strpos( $_SERVER['REQUEST_URI'], '/wp-admin/edit.php' ) !== false ) {
 		if ( ! current_user_can( 'activate_plugins' ) ) {
@@ -546,7 +542,7 @@ function remove_items_from_edit( $views ) {
 
 
 
-
+// Load More Posts
 function misha_my_load_more_scripts() {
 
 	global $wp_query;
@@ -630,16 +626,15 @@ function misha_loadmore_ajax_handler() {
 
 
 
+
+// Some Ajax for Image Upload, Reply and New Post
+
 add_action( 'wp_ajax_loadmore', 'misha_loadmore_ajax_handler' ); // wp_ajax_{action}
 add_action( 'wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler' ); // wp_ajax_nopriv_{action}
 
-
-
 /** Ajax Image Upload  */
 
-	/** Ajax Actions */
 	add_action( 'wp_ajax_sicomment_file_upload', 'si_comment_file_upload' );
-	/** Ajax Actions */
 
 function si_comment_file_upload() {
 	check_ajax_referer( 'file_upload', 'security' );
@@ -668,9 +663,7 @@ function si_get_image_for_return( $id, $size = 'full' ) {
 /** Ajax Image Upload  */
 
 /** Ajax New Post */
-	/** Ajax Actions */
 	add_action( 'wp_ajax_create_new_from_from_index', 'create_new_from_from_index' );
-	/** Ajax Actions */
 
 	function create_new_from_from_index(){
 		check_ajax_referer( 'new_post_sec', 'security' );
