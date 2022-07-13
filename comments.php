d235d4b5e1475bbe6bdec2e4debe033e32d141ba
sinanisler <sinan@sinanisler.com>
2022-07-13 23:35:20 +0300

    Update comments.php

--- comments.php
+++ comments.php
@@ -3,9 +3,9 @@
 <div id="comments" class="comments-area post-reply">
 	<?php if ( have_comments() ) { ?>
 		
-		<ul class="comment-list post-reply-list">
+		<ol class="comment-list post-reply-list">
 			<?php wp_list_comments( array( 'avatar_size' => 70, 'style' => 'ul', 'callback' => 'atarikafa_comments', 'type' => 'all' ) ); ?>
-		</ul>
+		</ol>
 
 		<?php the_comments_pagination( array( 'prev_text' => '<i class="fa fa-angle-left" aria-hidden="true"></i> <span class="screen-reader-text">' . __( 'Previous', 'atarikafa') . '</span>', 'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'atarikafa') . '</span> <i class="fa fa-angle-right" aria-hidden="true"></i>', ) ); ?>
 
