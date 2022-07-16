9ea41071ac6eba5babf362775fc97e6ba8d4aeda
Sinan <sinan@sinanisler.com>
2022-07-16 22:29:07 +0300

    this fixes #32

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <title><?php if(is_front_page() || is_home()){  bloginfo('name');    } else{ echo wp_title('');    }?></title>
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php bloginfo("stylesheet_directory"); ?>/style.css?v=81">
+    <link rel="stylesheet" href="<?php bloginfo("stylesheet_directory"); ?>/style.css?v=86">
     
 <?php wp_head(); ?>
 <?php if ( ! current_user_can( 'delete_others_posts' ) and !is_user_logged_in( ) ) { ?><style>#wpadminbar{display:none} html{margin-top: 0px !important;}</style><?php } ?>
