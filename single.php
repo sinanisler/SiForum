6a65edb4d795bd8fdf379542ab2b7d4cb6b9d678
Sinan <sinan@sinanisler.com>
2022-07-16 21:57:07 +0300

    this fixes #45

--- single.php
+++ single.php
@@ -194,8 +194,8 @@ jQuery(document).on('click',".editor-bold", function(){ jQuery('#comment').val(f
 jQuery(document).on('click',".editor-italic", function(){ jQuery('#comment').val(function(i, text) {  return text + '<i> Yatay </i>';   }); });
 jQuery(document).on('click',".editor-h2", function(){ jQuery('#comment').val(function(i, text) {  return text + '<h2> Başlık </h2>';   }); });
 jQuery(document).on('click',".editor-code", function(){ jQuery('#comment').val(function(i, text) {  return text + '<pre><code>\n \n</code></pre>'; }); });
-jQuery(document).on('click',".editor-link", function(){ jQuery('#comment').val(function(i, text) {  return text + '<a href="https://atarikafa.com"> LİNK TEXT </a>'; }); });
-jQuery(document).on('click',".editor-image", function(){ jQuery('#comment').val(function(i, text) {  return text + '<img src="https://atarikafa.com/foto.png">'; }); });
+jQuery(document).on('click',".editor-link", function(){ jQuery('#comment').val(function(i, text) {  return text + '<a href="https://google.com"> LİNK TEXT </a>'; }); });
+jQuery(document).on('click',".editor-image", function(){ jQuery('#comment').val(function(i, text) {  return text + '<img src="https://google.com/imagename.png">'; }); });
 jQuery(document).on('click',".upload-image", function(){ jQuery('#image_upload').trigger("click"); });
 jQuery(document).on('click',".editor-list", function(){ jQuery('#comment').val(function(i, text) {  return text + '<ul>'+'\n <li> bir </li>\n<li> iki </li>\n<li> üç </li> \n'+'</ul>';   }); });
 jQuery(document).on('click',".editor-mention", function(){ jQuery('#comment').val(function(i, text) {  return text + '@username';   }); });
