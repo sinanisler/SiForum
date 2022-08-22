57825145a0cb5ae637c7339864db6e3c36ccc3eb
Serkan Algur <info@wpadami.com>
2022-08-23 00:14:31 +0400

    First Options

--- class-siforumcustomizer.php
+++ class-siforumcustomizer.php
@@ -40,9 +40,103 @@ if ( ! class_exists( 'SiForumCustomizer' ) ) {
 			);
 			/* /---------SECTIONS---------- */
 			/* ----------SETTINGS---------- */
+			$wp_customize->add_setting(
+				'siforum_header_image',
+				array(
+					'type'              => 'theme_mod',
+					'transport'         => 'refresh',
+					'sanitize_callback' => array( $this, 'siforum_sanitize_image' ),
+				)
+			);
+			$wp_customize->add_setting(
+				'siforum_header_link',
+				array(
+					'type'      => 'theme_mod',
+					'default'   => 'https://atarikafa.com',
+					'transport' => 'refresh',
+				)
+			);
+			$wp_customize->add_setting(
+				'siforum_header_background_color',
+				array(
+					'type'      => 'theme_mod',
+					'transport' => 'refresh',
+					'default'   => '#c10c0c',
+				)
+			);
+			$wp_customize->add_setting(
+				'siforum_fontawesome_select',
+				array(
+					'type'      => 'theme_mod',
+					'transport' => 'refresh',
+					'default'   => 'no',
+				)
+			);
 			/* /---------SETTINGS---------- */
 			/* ----------CONTROLLERS------- */
+			$wp_customize->add_control(
+				new WP_Customize_Image_Control(
+					$wp_customize,
+					'siforum_header_image_control',
+					array(
+						'label'    => __( 'Header Logo', 'siforum' ),
+						'section'  => 'siforum_theme_settings',
+						'settings' => 'siforum_header_image',
+					)
+				)
+			);
+			$wp_customize->add_control(
+				'siforum_header_link',
+				array(
+					'id'      => 'siforum_header_link_control',
+					'label'   => 'Header Link',
+					'section' => 'siforum_theme_settings',
+				)
+			);
+			// Add Controls
+			$wp_customize->add_control(
+				new WP_Customize_Color_Control(
+					$wp_customize,
+					'siforum_header_background_color_control',
+					array(
+						'label'    => 'Header Background Color',
+						'section'  => 'siforum_theme_settings',
+						'settings' => 'siforum_header_background_color',
+
+					)
+				)
+			);
+
+			$wp_customize->add_control(
+				'siforum_fontawesome_select',
+				array(
+					'type'    => 'select',
+					'section' => 'siforum_theme_settings_fontawesome',
+					'label'   => __( 'Enable Font Awesome Usage', 'siforum' ),
+					'choices' => array(
+						'no'  => __( 'No' ),
+						'yes' => __( 'Yes' ),
+					),
+				)
+			);
 			/* /---------CONTROLLERS------- */
+		}
+
+
+		public function siforum_sanitize_image( $file, $setting ) {
+
+			$mimes = array(
+				'jpg|jpeg|jpe' => 'image/jpeg',
+				'gif'          => 'image/gif',
+				'png'          => 'image/png',
+				'svg'          => 'image/svg+xml',
+			);
+
+			//check file type from file name
+			$file_ext = wp_check_filetype( $file, $mimes );
+
+			//if file has a valid mime type return it, otherwise return default
+			return ( $file_ext['ext'] ? $file : $setting->default );
 		}
 
 	}
